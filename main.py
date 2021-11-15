76626a11c677bcc420cc808be7d7033cd8a313c6
ryudmilla <dave.ryu218@gmail.com>
2021-11-15 12:45:38 +0900

    change-background

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ def get_input():
     try:
         input = stdin.readline().strip()
         if input in phones:
-            width = 750
-            height = 1334
+            width,height = 750, 1334
         else:
             width,height = map(int, input.split())
         return (width,height)
@@ -24,16 +23,26 @@ def paste():
         elif location == 'bottom':
             position = ((width - image.width)//2,(height - image.height))
             break
-        elif location == 'upper':
+        elif location == 'lower':
             position = ((width - image.width)//2,(int)((height - image.height)*0.75))
             break
         elif location == 'top':
             position = ((width - image.width)//2,0)
             break
-        elif location == 'lower':
+        elif location == 'upper':
             position = ((width - image.width)//2,(int)((height - image.height)*0.25))
             break
     Image.Image.paste(background,image,position)
+
+def change_background():
+    background_color = image.getpixel((0,0))
+
+    for x in range(0,width):
+        for y in range(0,height):
+            background.putpixel((x,y),background_color)
+
+def resize():
+    pass
 
 # print("iPhone 8 : 750 × 1334")
 phones = ['iphone8']
@@ -43,7 +52,11 @@ size = (width,height) = get_input()
 background = Image.new(mode='RGB',size=size, color=(256,256,256))
 
 image = Image.open('logo.png')
+image.convert('RGB')
 print(image.size)
 
+change_background()
+
 paste()
+
 background.save('background.png')
