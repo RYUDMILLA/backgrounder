a853491149996bf3dbcdbaf20fef59ddc939a04e
ryudmilla <dave.ryu218@gmail.com>
2021-11-22 17:39:17 +0900

    fix-converting

--- JPEG_to_PNG.py
+++ JPEG_to_PNG.py
@@ -1,9 +1,15 @@
 from PIL import Image
 
+def det_range(pixel, background):
+    for i in range(len(background)):
+        if abs(background[i] - pixel[i]) > int(255*0.05): # 5% range
+            return False
+    return True
+
 def change_background(img, color, background_color):
     for x in range(0,img.width):
         for y in range(0,img.height):
-            if img.getpixel((x,y)) == background_color:
+            if det_range(img.getpixel((x,y)),background_color) == True:
                 img.putpixel((x,y),color)
 
 def background_to_transparent(img):
@@ -11,10 +17,10 @@ def background_to_transparent(img):
     print(background_color)
     change_background(img, (0,0,0,0), background_color)
 
-def JPEG_to_PNG():
-    img = Image.open('intj.jpg').convert('RGBA')
+def JPEG_to_PNG(name):
+    img = Image.open(f'{name}.jpg').convert('RGBA')
     print("Change background to transparent?(Y/N)",end=' ')
     yn = input()
     if yn.lower() == 'y':
         background_to_transparent(img)
-    img.save('sample.png')
+    img.save(f'{name}.png')
--- PNG_to_JPEG.py
+++ PNG_to_JPEG.py
@@ -1,23 +1,23 @@
 from PIL import Image
 
-def to_white(image):
+def to_white(image, name):
     img = Image.open(image)
     img.load()
     bg = Image.new(mode='RGB',size=img.size,color=(255,255,255))
     bg.paste(img,mask=img.split()[3])
-    bg.save('sample.jpg',"JPEG",quality=100)
+    bg.save(f'{name}.jpg',"JPEG",quality=100)
 
-def to_black(image):
+def to_black(image, name):
     img = Image.open(image).convert('RGB')
-    img.save('sample.jpg')
+    img.save(f'{name}.jpg')
 
-def PNG_to_JPEG():
-    image = 'logo.png'
+def PNG_to_JPEG(name):
+    image = f'{name}.png'
     print("Transparent to White? to Black?")    
     wb = input()
     if wb.lower() == 'white':
-        to_white(image)
+        to_white(image, name)
     elif wb.lower() == 'black':
-        to_black(image)
+        to_black(image, name)
     else:
-        to_white(image)
+        to_white(image, name)
